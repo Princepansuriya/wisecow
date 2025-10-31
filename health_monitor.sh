931b522e55f8b567fde0404e054b189f03281bd8
prince <princepansuriya1616@gmail.com>
2025-10-31 19:49:35 +0530

    update

--- health_monitor.sh
+++ health_monitor.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 
-# Thresholds
-CPU_THRESHOLD=80
-MEM_THRESHOLD=80
-DISK_THRESHOLD=80
+LOGFILE="system_health.log"
 
-LOG_FILE="./system_health.log"
+CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+MEM=$(free | awk '/Mem/ {printf "%.2f", $3/$2 * 100}')
+DISK=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
 
-cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
-mem_usage=$(free | awk '/Mem/ {printf("%.0f"), $3/$2 * 100}')
-disk_usage=$(df / | awk '/\// {print $5}' | sed 's/%//')
-
-echo "CPU: $cpu_usage% | Memory: $mem_usage% | Disk: $disk_usage%" | tee -a $LOG_FILE
-
-if (( ${cpu_usage%.*} > CPU_THRESHOLD )); then
-    echo "⚠ ALERT: CPU usage high!" | tee -a $LOG_FILE
-fi
-
-if (( mem_usage > MEM_THRESHOLD )); then
-    echo "⚠ ALERT: Memory usage high!" | tee -a $LOG_FILE
-fi
-
-if (( disk_usage > DISK_THRESHOLD )); then
-    echo "⚠ ALERT: Disk usage high!" | tee -a $LOG_FILE
-fi
+echo "CPU: $CPU% | Memory: $MEM% | Disk: $DISK%" | tee -a $LOGFILE
